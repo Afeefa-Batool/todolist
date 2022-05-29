45a8b9f0770697be6663f4f172736ea621e24cb4
Afeefa-Batool <99643705+Afeefa-Batool@users.noreply.github.com>
2022-05-28 22:56:54 -0700

    Todo-List Application

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+       <div className='header'>
+           <h1>
+           Todo-List
+           </h1>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Header
